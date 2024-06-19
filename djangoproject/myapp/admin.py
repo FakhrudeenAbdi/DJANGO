b9f1a1dad583e0bbf36304a383b983590786e9ea
FakhrudeenAbdi <fakhrudinabdi751@gmail.com>
2024-06-19 11:18:39 +0300

    first commit

--- djangoproject/myapp/admin.py
+++ djangoproject/myapp/admin.py
@@ -4,3 +4,9 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+
+
+from .models import Post, Editor
+
+admin.site.register(Editor)
--- djangoproject/myapp/models.py
+++ djangoproject/myapp/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+class Editor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
 
-
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    editor = models.ForeignKey(Editor, on_delete=models.SET_NULL, null=True, related_name='posts')
 
-    
     def __str__(self):
-      return self.title
+        return self.title
+
